aaa48583046a6e83261bdea39471bf764797ee51
Rehan Ali <rehan.shaukat@epsystems.pk>
2021-06-13 19:02:39 +0500

    Fix Automation Script and update utilities

--- os-setup-script.sh
+++ os-setup-script.sh
@@ -34,15 +34,16 @@ curl -sSL https://get.rvm.io | bash -s stable
 
 echo "Installing Additional Software / Utilities and configuration..."
 # SDK Manager
-sdk i java 8.0.252.hs-adpt
-sdk i java 11.0.7.hs-adpt
-sdk i java 14.0.1.hs-adpt
-sdk i java 15.0.0.hs-adpt
-sdk u java 8.0.252.hs-adpt
+sdk i java 8.0.292.hs-adpt
+sdk i java 11.0.11.hs-adpt
+sdk i java 16.0.1-open
+sdk u java 11.0.11.hs-adpt
 # Ruby
-rvm install ruby-head
-rvm install ruby-2.6.5
-rvm use 2.7.0
+
+rvm install ruby-2.6
+rvm install ruby-2.7
+rvm use 2.7
+
 sudo gem install cocoapods
 sudo gem install cocoapods-keys
 # Node
@@ -59,20 +60,39 @@ chsh -s /usr/local/bin/zsh
 echo "Installing PowerLevel10K"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
+echo "Installing ZSH Syntax Highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Installing ZSH 256 Color Plougin"
+git clone https://github.com/chrissicool/zsh-256color.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-256color
+
+echo "Installing ZSH Auto Suggestiong plugin"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
 echo "Installing Flutter"
 git clone https://github.com/flutter/flutter.git ~/.flutter -b beta
+export PATH="$HOME/.flutter/bin:$PATH"
+
+echo "Configuring Flutter"
+flutter precache
+
+echo "Flutter Channel"
+flutter channel
+
+echo "Flutter Doctor"
+flutter doctor -v
 
 echo "Copying configuration file to Home Directory..."
 files="gitconfig hyper.js p10k.zsh zsh_history zshrc"
 
 for file in ${files}; do
+    echo "Removing $file if exists"
+    if test -f "~/.${file}" then
+        rm -f ~/.${file}
+    fi
     echo "Copying $file in home directory."
     cp -af ./config/.${file} ~/.${file}
 done
-
-source ~/.zshrc
-
-echo "Configuring Flutter"
-flutter precache
 
 echo "Macbook setup completed!"
