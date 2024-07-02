707ab9df01c744d2fca126e9ad262ca48ca85129
Rehan Ali <rehannali@programmer.net>
2024-07-02 17:10:06 +0500

    starting to migrate to ansible and make new folder structure.

--- supported-executable/pre-setup.sh
+++ supported-executable/pre-setup.sh
@@ -2,34 +2,35 @@ function presetup_execution {
     install_xcode_cli
     check_if_you_want_to_continue
     install_homebrew
+    setup_ansible
 }
 
 
 function install_xcode_cli {
-    read -n 1 -p "Are you sure you want to install Xcode CLI if not already? <y/N> :" yn
-
-    if [[ ${yn} =~ [yY] ]]; then
-        info "Installing Xcode CLI"
+    if [[ ! $(xcode-select -p 2>/dev/null) ]]; then
+        info "Installing Xcode CLI..."
         run xcode-select --install
+        # Wait for Xcode CLI to be installed
+        until xcode-select -p &>/dev/null; do
+            info "Waiting for Xcode CLI to install..."
+            sleep 5
+        done
+    else
+        info "Xcode CLI is already installed."
     fi
-
-    linebreak
 }
 
 function check_if_you_want_to_continue {
-    read -n 1 -p "Are you wish to continue to proceed setup after xcode CLI installation <y/N> :" ans
-
-    if [[ ! ${ans} =~ [yY] ]]; then
-        bail "Exiting remaining installation"
-    fi
-
-    linebreak
-
     # Add initial configuration for M1 MacBook's
     if [[ $(uname -m) == 'arm64' ]]; then
         info "Configuring Settings for M1 MacBook's"
         run export PATH="/opt/homebrew/bin:$PATH"
-        run sudo softwareupdate --install-rosetta --agree-to-license
+        if ! /usr/bin/pgrep oahd >/dev/null 2>&1; then
+            info "Installing Rosetta 2..."
+            run sudo softwareupdate --install-rosetta --agree-to-license
+        else
+            info "Rosetta 2 is already installed."
+        fi
     fi
 }
 
@@ -38,5 +39,16 @@ function install_homebrew {
     if ! command -v brew &> /dev/null; then
         info "Installing homebrew..."
         run /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+    else
+        info "Homebrew is already installed."
+    fi
+}
+
+function setup_ansible {
+    if ! command -v ansible &> /dev/null; then
+        info "Installing Ansible..."
+        brew install ansible
+    else
+        info "Ansible is already installed."
     fi
 }
